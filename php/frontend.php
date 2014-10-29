086e70eefc6d7d2af05ddb926ed547ec4b6d4335
Jonathan <modjow.jm9@gmail.com>
2014-10-29 12:20:52 +0100

    Debug : try get JSON input

--- php/frontend.php
+++ php/frontend.php
@@ -1,6 +1,9 @@
 <?php
 	require 'lib/autoload.php';
 	
+
+	echo json_decode(file_get_contents('php://input'), true);
+
 	// INSTANCIATION DE l'APPLICATION
 	$app = new app\frontend\ApplicationFrontend();
 	$app->run();
