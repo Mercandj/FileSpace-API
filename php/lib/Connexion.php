72db136f03d338600b898df24469b92135b98ccf
Jonathan <modjow.jm9@gmail.com>
2014-10-29 13:55:25 +0100

    Debug PDO

--- php/lib/Connexion.php
+++ php/lib/Connexion.php
@@ -6,10 +6,10 @@ class Connexion{
 
 	public function __construct(Application $app){
 
-		echo $app->_config->get('bdd_name').'   '.$app->_config->get('bdd_login').'   '.$app->_config->get('bdd_password');
+		//echo $app->_config->get('bdd_name').'   '.$app->_config->get('bdd_login').'   '.$app->_config->get('bdd_password');
 
 		try{
-			$this->_pdo = new \PDO('mysql:host=localhost;dbname='.$app->_config->get('bdd_name'), $app->_config->get('bdd_login'), $app->_config->get('bdd_password'));
+			$this->_pdo = new PDO('mysql:host=localhost;dbname='.$app->_config->get('bdd_name'), $app->_config->get('bdd_login'), $app->_config->get('bdd_password'));
 		}catch(Exception $e){
 		    die('Erreur : '.$e->getMessage());
 		}
