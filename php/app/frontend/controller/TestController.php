440ea96340ce73ee111d93cf9900c2ab768615fa
Jonathan <modjow.jm9@gmail.com>
2014-10-29 02:57:50 +0100

    PHP : Add view

--- php/app/frontend/controller/TestController.php
+++ php/app/frontend/controller/TestController.php
@@ -5,5 +5,8 @@ class CategorieController extends \lib\Controller{
 
 	public function getCategories(){
 		
+
+		// SEND PAGE
+		$this->_app->_HTTPResponse->send($this->_app->_page->draw('TestJSON.php'));
 	}
 }
---
+++ php/app/frontend/view/TestJSON.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<?php include("app/frontend/view/head.php") ?>
+	<body>
+		
+		<p>{"COUCOU":"cc"}</p>
+
+
+
+
+	</body>
+</html>
