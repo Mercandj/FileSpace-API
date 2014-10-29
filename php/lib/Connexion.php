a165dd027c4fd58bc7399aad28a7cb7afa7ea34c
Jonathan <modjow.jm9@gmail.com>
2014-10-29 13:54:07 +0100

    Debug PDO

--- php/lib/Connexion.php
+++ php/lib/Connexion.php
@@ -5,6 +5,9 @@ class Connexion{
 	private $_pdo;
 
 	public function __construct(Application $app){
+
+		echo $app->_config->get('bdd_name').'   '.$app->_config->get('bdd_login').'   '.$app->_config->get('bdd_password');
+
 		try{
 			$this->_pdo = new \PDO('mysql:host=localhost;dbname='.$app->_config->get('bdd_name'), $app->_config->get('bdd_login'), $app->_config->get('bdd_password'));
 		}catch(Exception $e){
