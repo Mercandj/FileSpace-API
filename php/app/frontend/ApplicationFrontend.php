06e941819405ccf852279e24b998b0d668c0426e
Jonathan <modjow.jm9@gmail.com>
2014-10-30 00:10:31 +0100

    Debug

--- php/app/frontend/ApplicationFrontend.php
+++ php/app/frontend/ApplicationFrontend.php
@@ -1,6 +1,5 @@
 <?php
 namespace app\frontend;
-use \lib\Entities\User;
 
 class Applicationfrontend extends \lib\Application {
 	protected $_name;
@@ -12,24 +11,11 @@ class Applicationfrontend extends \lib\Application {
 
 	public function run() {
 
-
 		$controlleur = new \app\frontend\controller\LoginController($this);
 		
 		if($controlleur->isUser() || $this->getController() instanceof \app\frontend\controller\RegisterController)
 			$this->getController()->exec();
-		//else
-
-
-		/*
-		if(!$this->_session->isLogin()){
-			$controlleur = new \app\frontend\controller\UserController($this);
-			$controlleur->login();
-
-		}else{
-		*/
-			//$this->_page->assign('header',$this->_config->get("title"));				
-		//$this->getController()->exec();
-		//}
+		
 		exit();
 	}
 }
