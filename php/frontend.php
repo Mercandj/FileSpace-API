c2ecbd2526843c51449dc275748f7ef32b06d92c
Jonathan <modjow.jm9@gmail.com>
2014-10-29 12:22:39 +0100

    Debug

--- php/frontend.php
+++ php/frontend.php
@@ -2,7 +2,9 @@
 	require 'lib/autoload.php';
 	
 
-	echo json_decode(file_get_contents('php://input'), true);
+	echo 'json_decode '.json_decode(file_get_contents('php://input'), true);
+
+	echo 'file_get_contents '.file_get_contents('php://input');
 
 	// INSTANCIATION DE l'APPLICATION
 	$app = new app\frontend\ApplicationFrontend();
