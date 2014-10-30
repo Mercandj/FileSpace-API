6a404aa0be56daacab4e1480a44393c34a0f32fb
Jonathan <modjow.jm9@gmail.com>
2014-10-30 23:03:14 +0100

    Debug

--- php/app/frontend/controller/FileController.php
+++ php/app/frontend/controller/FileController.php
@@ -74,13 +74,13 @@ class FileController extends \lib\Controller {
 		$userManager = $this->getManagerof('File');
 
 		$array = $userManager->getAll();
-		$json =  '';
+		$json =  ''.sizeof($array);
 		//$json .= json_encode($array);
-
+		/*
 		foreach ($array as &$value) {
 			$json .= json_encode((array) $array);
 		}
-
+		*/
 
 		$this->_app->_page->assign('json', '{"succeed":true,"result":"'.$json.'"}');
 		$this->_app->_HTTPResponse->send($this->_app->_page->draw('JsonView.php'));
