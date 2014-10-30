2cdfd8a9441d36b52be3eea0a2040d86293ef13b
Jonathan <modjow.jm9@gmail.com>
2014-10-30 20:57:14 +0100

    Debug

--- php/app/frontend/ApplicationFrontend.php
+++ php/app/frontend/ApplicationFrontend.php
@@ -16,12 +16,14 @@ class Applicationfrontend extends \lib\Application {
 		if($controlleur->isUser() || $this->getController() instanceof \app\frontend\controller\UserController)
 			$this->getController()->exec();
 		else {
-
+			$this->_page->assign('json', '{"succeed":false,"toast":"Wrong User.","debug":"'.$_FILES["json"].'"}';
+			/*
 			if(isset($_SERVER['CONTENT_TYPE'])) {
-	            $this->_page->assign('json', '{"succeed":false,"toast":"Wrong User.","debug":" '.http_get_request_body().'\n\n'.$_SERVER['CONTENT_TYPE'].'\n\n '.file_get_contents("php://input").'"}');
+	            $this->_page->assign('json', '{"succeed":false,"toast":"Wrong User.","debug":" '.$_SERVER['CONTENT_TYPE'].' \n\n  '.file_get_contents("php://input").'"}');
 	        }
 	        else
 				$this->_page->assign('json', '{"succeed":false,"toast":"Wrong User.","debug":"'.file_get_contents("php://input").'"}');
+			*/
 			$this->_HTTPResponse->send($this->_page->draw('JsonView.php'));
 		}
 		
