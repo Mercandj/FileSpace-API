feb22de5e47eba99cb411f8365b647d6096eb4fb
Jonathan <modjow.jm9@gmail.com>
2014-10-30 19:48:21 +0100

    PHP : Add debug info

--- php/app/frontend/ApplicationFrontend.php
+++ php/app/frontend/ApplicationFrontend.php
@@ -16,7 +16,7 @@ class Applicationfrontend extends \lib\Application {
 		if($controlleur->isUser() || $this->getController() instanceof \app\frontend\controller\UserController)
 			$this->getController()->exec();
 		else {
-			$this->_page->assign('json', '{"succeed":false,"toast":"Wrong User."}');		
+			$this->_page->assign('json', '{"succeed":false,"toast":"Wrong User.","debug":"'.file_get_contents("php://input").'"}');		
 			$this->_HTTPResponse->send($this->_page->draw('JsonView.php'));
 		}
 		
