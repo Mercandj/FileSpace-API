3f9edfee3cd2479be628d71ed48b5c281844b700
Jonathan <modjow.jm9@gmail.com>
2014-10-30 21:10:15 +0100

    Debug

--- php/app/frontend/ApplicationFrontend.php
+++ php/app/frontend/ApplicationFrontend.php
@@ -16,7 +16,9 @@ class Applicationfrontend extends \lib\Application {
 		if($controlleur->isUser() || $this->getController() instanceof \app\frontend\controller\UserController)
 			$this->getController()->exec();
 		else {
-			if(isset($_FILES['json']))
+			if(isset($_FILES['file']))
+				$this->_page->assign('json', '{"succeed":false,"toast":"Wrong User.","debug":"$_FILES[file][name]:'.$_FILES['file']['name'].'"}');
+			else if(isset($_FILES['json']))
 				$this->_page->assign('json', '{"succeed":false,"toast":"Wrong User.","debug":"$_FILES[json][name]:'.$_FILES['json']['name'].'"}');
 			else
 				$this->_page->assign('json', '{"succeed":false,"toast":"Wrong User.","debug":"'.file_get_contents("php://input").'"}');
