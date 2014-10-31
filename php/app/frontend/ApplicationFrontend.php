4aa17280b6389d4f0b6c239050ef4b8de6748b2e
Jonathan <modjow.jm9@gmail.com>
2014-10-31 14:11:21 +0100

    PHP clean code

--- php/app/frontend/ApplicationFrontend.php
+++ php/app/frontend/ApplicationFrontend.php
@@ -16,23 +16,7 @@ class Applicationfrontend extends \lib\Application {
 		if($controlleur->isUser() || $this->getController() instanceof \app\frontend\controller\UserController)
 			$this->getController()->exec();
 		else {
-			/*
-			if(isset($_FILES['file']))
-				$this->_page->assign('json', '{"succeed":false,"toast":"Wrong User.","debug":"$_FILES[file][name]:'.$_FILES['file']['name'].'\n\n$_POST[json]:'.$_POST['json'].'"}');
-			else if(isset($_FILES['json']))
-				$this->_page->assign('json', '{"succeed":false,"toast":"Wrong User.","debug":"$_FILES[json][name]:'.$_FILES['json']['name'].'"}');
-			else
-				$this->_page->assign('json', '{"succeed":false,"toast":"Wrong User.","debug":"COUCOU'.file_get_contents("php://input").'"}');
-
-			/*
-			if(isset($_SERVER['CONTENT_TYPE'])) {
-	            $this->_page->assign('json', '{"succeed":false,"toast":"Wrong User.","debug":" '.$_SERVER['CONTENT_TYPE'].' \n\n  '.file_get_contents("php://input").'"}');
-	        }
-	        else
-				$this->_page->assign('json', '{"succeed":false,"toast":"Wrong User.","debug":"'.file_get_contents("php://input").'"}');
-			*/
-
-			$json = '{"succeed":false,"toast":"Wrong User."}';
+			$this->_app->_page->assign('json', '{"succeed":false,"toast":"Wrong User."}');
 			$this->_HTTPResponse->send($this->_page->draw('JsonView.php'));
 		}
 		
