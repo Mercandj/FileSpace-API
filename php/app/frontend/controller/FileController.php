dcc4f9cc4ea77e2718c6a89e627d1bc39e20c634
Jonathan <modjow.jm9@gmail.com>
2014-10-31 23:47:28 +0100

    PHP : add ddl (prototype)

--- php/app/frontend/controller/FileController.php
+++ php/app/frontend/controller/FileController.php
@@ -110,4 +110,56 @@ class FileController extends \lib\Controller {
 		$this->_app->_page->assign('json', '{"succeed":true,"result":'.json_encode($json).'}');
 		$this->_app->_HTTPResponse->send($this->_app->_page->draw('JsonView.php'));
 	}
+
+	public function ddl() {
+		$root = __DIR__."\\..\\..\\..\\public\\";
+
+		if(!@array_key_exists('file', $this->_app->_parameters)) {
+			$json = '{"succeed":false,"toast":"FileController : file key (json) not found."}';
+			$this->_app->_page->assign('json', $json);
+			$this->_app->_HTTPResponse->send($this->_app->_page->draw('JsonView.php'));
+			return;
+		}
+
+		$file = new File($this->_app->_parameters['file']);
+		$userManager = $this->getManagerof('File');
+
+		if($userManager->exist($file->getUrl())) {
+			$file_name = $root . $file->getUrl();
+
+			if(is_file($file_name)) {
+				// required for IE
+				//if(ini_get('zlib.output_compression')) { ini_set('zlib.output_compression', 'Off');	}
+
+				// get the file mime type using the file extension
+				switch(strtolower(substr(strrchr($file_name, '.'), 1))) {
+					case 'pdf': $mime = 'application/pdf'; break;
+					case 'zip': $mime = 'application/zip'; break;
+					case 'jpeg':
+					case 'jpg': $mime = 'image/jpg'; break;
+					default: $mime = 'application/force-download';
+				}
+				header('Pragma: public'); 	// required
+				header('Expires: 0');		// no cache
+				header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+				header('Last-Modified: '.gmdate ('D, d M Y H:i:s', filemtime ($file_name)).' GMT');
+				header('Cache-Control: private',false);
+				header('Content-Type: '.$mime);
+				header('Content-Disposition: attachment; filename="'.basename($file_name).'"');
+				header('Content-Transfer-Encoding: binary');
+				header('Content-Length: '.filesize($file_name));	// provide file size
+				header('Connection: close');
+				readfile($file_name);		// push it out
+			}
+			else {
+				$this->_app->_page->assign('json', '{"succeed":false,"result":"Physic : Bad File url."}');
+				$this->_app->_HTTPResponse->send($this->_app->_page->draw('JsonView.php'));
+			}
+
+		}
+		else {
+			$this->_app->_page->assign('json', '{"succeed":false,"result":"Bdd : Bad File url."}');
+			$this->_app->_HTTPResponse->send($this->_app->_page->draw('JsonView.php'));
+		}
+	}
 }
