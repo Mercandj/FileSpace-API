1e1f6e4b34570206ab5e9f9b7f374a87f70ed799
Jonathan <modjow.jm9@gmail.com>
2014-10-31 13:28:05 +0100

    Clean code

--- php/app/frontend/controller/FileController.php
+++ php/app/frontend/controller/FileController.php
@@ -43,40 +43,45 @@ class FileController extends \lib\Controller {
 		
 		$target_dir = $root . basename( $_FILES["file"]["name"]);
 
-		$extensions_valides = array( 'rar', 'zip', 'apk', 'png', 'jpg', 'jpeg', 'gif', 'png', 'txt', 'mp3', 'avi', 'pdf', 'docx', 'pptx' );
+		$extensions_valides = array( 'rar', 'zip', 'apk', 'png', 'jpg', 'jpeg', 'gif', 'png', 'txt', 'mp3', 'avi', 'mp4', 'pdf', 'docx', 'pptx' );
 		$extension_upload = strtolower(  substr(  strrchr($_FILES['file']['name'], '.')  ,1)  );
+
+		$succeed = false;
+		$toast = '';
 
 		if(array_key_exists('file', $_FILES)) {
 	    	if ($_FILES['file']['error'] === UPLOAD_ERR_OK) {
 				if(!$userManager->exist($file->getUrl())) {
 					if ( in_array($extension_upload,$extensions_valides) ) {
-						if ( 0 < $_FILES['file']['size'] && $_FILES['file']['size'] < 100000000  ) {
+						if ( 0 < $_FILES['file']['size'] && $_FILES['file']['size'] < 800000000  ) {
 							if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_dir)) {
 
 								$file->setId(uniqid());
 								$file->setSize($_FILES['file']['size']);
 								$userManager->add($file);
 
-					    		$this->_app->_page->assign('json', '{"succeed":true,"toast":"The file '. basename( $_FILES["file"]["name"]) .' has been uploaded."}');
+								$succeed = true;
+					    		$toast = 'The file '. basename( $_FILES["file"]["name"]) .' has been uploaded.';
 							}
 							else
-								$this->_app->_page->assign('json', '{"succeed":false,"toast":"Sorry, there was an error uploading your file."}');
+								$toast = 'Sorry, there was an error uploading your file.';
 					    }
 						else
-							$this->_app->_page->assign('json', '{"succeed":false,"toast":"File size : '.$_FILES['file']['size'].'"}');
+							$toast = 'File size : '.$_FILES['file']['size'].'.';
 					}
 					else
-						$this->_app->_page->assign('json', '{"succeed":false,"toast":"Bad extension."}');
+						$toast = 'Bad extension.';
 				}
 				else
-					$this->_app->_page->assign('json', '{"succeed":false,"toast":"File exists."}');
+					$toast = 'File exists.';
 			}
 			else
-				$this->_app->_page->assign('json', '{"succeed":false,"toast":"Upload failed with error code '.$_FILES['file']['error'].'."}');
+				$toast = 'Upload failed with error code '.$_FILES['file']['error'].'.';
 		}
 		else
-			$this->_app->_page->assign('json', '{"succeed":false,"toast":"Upload failed : !array_key_exists(file, $_FILES)."}');
+			$toast = 'Upload failed : !array_key_exists(file, $_FILES).';
 
+		$this->_app->_page->assign('json', '{"succeed":'.$succeed.',"toast":"'.$toast.'"}');
 		$this->_app->_HTTPResponse->send($this->_app->_page->draw('JsonView.php'));		
 	}
 
