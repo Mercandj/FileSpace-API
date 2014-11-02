8aa41f1f906af7bb485cd5eced49220bffa3cc9d
Jonathan <modjow.jm9@gmail.com>
2014-11-02 21:47:46 +0100

    DialogInitial : Scrolling in landscape

--- src/com/mercandalli/jarvis/fragment/FileManagerFragmentLocal.java
+++ src/com/mercandalli/jarvis/fragment/FileManagerFragmentLocal.java
@@ -25,7 +25,7 @@ public class FileManagerFragmentLocal extends Fragment {
 	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_filemanager_fragment, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_filemanager_online, container, false);
         
         
         return rootView;
--- src/com/mercandalli/jarvis/fragment/FileManagerFragmentServer.java
+++ src/com/mercandalli/jarvis/fragment/FileManagerFragmentServer.java
@@ -41,7 +41,7 @@ public class FileManagerFragmentServer extends Fragment {
 	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {		
-        View rootView = inflater.inflate(R.layout.fragment_filemanager_fragment, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_filemanager_online, container, false);
         circulerProgressBar = (ProgressBar) rootView.findViewById(R.id.circulerProgressBar);
         files = (ListView) rootView.findViewById(R.id.files);
         
