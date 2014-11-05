485341092358d7923adef713ea656752fe428605
Jonathan <modjow.jm9@gmail.com>
2014-11-05 11:36:34 +0100

    PHP : Add switch($_SERVER['REQUEST_METHOD'])

--- php/lib/HTTPRequest.php
+++ php/lib/HTTPRequest.php
@@ -8,19 +8,47 @@ class HTTPRequest{
 		$this->_app = $app;
 	}
 
-	public function postData($key){
+	private function postData($key){
 		return isset($_POST[$key]) ? $_POST[$key] : null;
 	}
 
-	public function postExist($key){
+	private function postExist($key){
 		return isset($_POST[$key]);
 	}
 
-	public function getData($key){
+	private function getData($key){
 		return isset($_GET[$key]) ? $_GET[$key] : null;
 	}
 
-	public function getExist($key){
+	private function getExist($key){
 		return isset($_GET[$key]);
+	}
+
+	public function exist($key) {
+		switch($_SERVER['REQUEST_METHOD']) {		
+		case 'GET': 
+		if(getExist($key))
+			return true;
+		break;
+		case 'POST': 
+		if(postExist($key))
+			return true;
+		break;
+		}
+		return false;
+	}
+
+	public function get($key) {
+		switch($_SERVER['REQUEST_METHOD']) {		
+		case 'GET':
+		if(getExist($key))
+			return getData($key);
+		break;
+		case 'POST': 
+		if(postExist($key))
+			return json_decode(postData($key), true);
+		break;
+		}
+		return null;
 	}
 }
