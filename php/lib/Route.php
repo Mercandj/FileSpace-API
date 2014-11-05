70348043e071d75d0c91f71d2f6307c45ea6f5d0
Jonathan <modjow.jm9@gmail.com>
2014-11-05 11:43:16 +0100

    PHP : change matching router

--- php/lib/Route.php
+++ php/lib/Route.php
@@ -5,12 +5,14 @@ class Route{
 	private $_url,
 		$_controller,
 		$_action,
+		$method,
 		$_matches;
 
-	public function __construct($url,$controller,$action, $matches){
+	public function __construct($url, $controller, $action, $method, $matches){
 		$this->_url = $url;
 		$this->_controller= $controller;
 		$this->_action = $action;
+		$this->method = $method;
 		$this->_matches = $matches;
 	}
 
@@ -28,5 +30,9 @@ class Route{
 
 	public function getAction(){
 		return $this->_action;
+	}
+
+	public function getMethod(){
+		return $this->method;
 	}
 }
--- php/lib/Router.php
+++ php/lib/Router.php
@@ -13,8 +13,8 @@ class Router{
 		$parsed = json_decode( file_get_contents(__DIR__.'/../config/routes.json') );
 
 		foreach($parsed as $route){
-			if( ($matches = self::match($route->{'url'},$url)) ){
-				return new Route($route->{'url'}, $route->{'controller'}, $route->{'action'}, $matches);
+			if( ($matches = self::match($route->{'url'},$url,$route->{'method'},$_SERVER['REQUEST_METHOD'])) ){
+				return new Route($route->{'url'}, $route->{'controller'}, $route->{'action'}, $route->{'method'}, $matches);
 			}
 		}
 		
@@ -27,11 +27,10 @@ class Router{
 	*	@param url client
 	*	@return boolean
 	*/
-	private static function match($url_json,$url_client){
-		if(preg_match('`^'.$url_json.'$`', $url_client, $matches)){
-			return explode("/",$matches[0]);
-		}else{
-			return false;
-		}
+	private static function match($url_json,$url_client,$method_json,$method_client){
+		if ($method_json === $method_client)
+			if(preg_match('`^'.$url_json.'$`', $url_client, $matches))
+				return explode("/",$matches[0]);
+		return false;
 	}
 }
