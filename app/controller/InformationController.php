430d9329033a657aa5d92d23d7d09ea157e93cd2
Jonathan <modjow.jm9@gmail.com>
2014-12-09 15:05:55 +0100

    Minor

--- app/controller/InformationController.php
+++ app/controller/InformationController.php
@@ -14,11 +14,15 @@ class InformationController extends \lib\Controller {
 	 */
 	public function get() {
 
-		$json['succeed'] = true;
-		$json['php_uname'] = php_uname();
-		$json['memory_get_usage'] = memory_get_usage();
 		$fileManager = $this->getManagerof('File');
-		$json['sizeAll'] = $fileManager->sizeAll();
+		$json['succeed'] = true;
+
+		$json['result'] = array(
+			'php_uname' => php_uname(),
+			'php_uname' => php_uname(),
+			'memory_get_usage' => memory_get_usage(),
+			'sizeAll' => $fileManager->sizeAll()
+		);
 
 		HTTPResponse::send(json_encode($json));
 	}
