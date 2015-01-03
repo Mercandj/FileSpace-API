34c9c2f7c2635f7b52515f0446db8892187d4c7a
Jonathan <modjow.jm9@gmail.com>
2015-01-03 22:27:37 +0100

    Minor

--- bdd/script.sql
+++ bdd/script.sql
@@ -56,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `File` (
   `type` VARCHAR(60) NULL,
   `number_read` INT UNSIGNED NULL,
   `number_download` INT UNSIGNED NULL,
-  `folder` BOOLEAN DEFAULT FALSE,
+  `folder` BOOLEAN NOT NULL,
   `id_User` INT NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = MyISAM;
