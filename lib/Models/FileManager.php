cbcf0618bf93d21aaa7eb600ff91791e9c334cb9
Jonathan <modjow.jm9@gmail.com>
2015-01-04 18:12:07 +0100

    Test

--- lib/Models/FileManager.php
+++ lib/Models/FileManager.php
@@ -16,6 +16,10 @@ class FileManager extends \lib\Manager {
 
 		$req = $this->_db->prepare('INSERT INTO file(url,size,visibility,date_creation,id_User,type,directory) VALUES (:url, :size, :visibility, :date_creation, :id_User, :type, :directory)');
 		$req->bindParam(':url',$url,\PDO::PARAM_STR);
+
+		$echo("COUCOU ".$size);
+
+
 		$req->bindParam(':size',$size,\PDO::PARAM_INT);
 		$req->bindParam(':visibility',$visibility,\PDO::PARAM_INT);
 		$req->bindParam(':date_creation',$date_creation,\PDO::PARAM_STR);
