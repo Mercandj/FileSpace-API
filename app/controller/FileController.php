d6eb9ddfcdcbe2cc0b00d7010280cbaac71f444c
Jonathan <modjow.jm9@gmail.com>
2015-01-04 17:55:08 +0100

    Test

--- app/controller/FileController.php
+++ app/controller/FileController.php
@@ -63,6 +63,7 @@ class FileController extends \lib\Controller {
 				'visibility' => $visibility,
 				'date_creation' => date('Y-m-d H:i:s'),
 				'id_user' => 1,
+				'size' => 0,
 				'type' => '.dir',
 				'directory' => 1
 			));
@@ -123,6 +124,7 @@ class FileController extends \lib\Controller {
 				'visibility' => $visibility,
 				'date_creation' => date('Y-m-d H:i:s'),
 				'id_user' => 1,
+				'size' => 0,
 				'type' => $extension_upload
 			));
 
