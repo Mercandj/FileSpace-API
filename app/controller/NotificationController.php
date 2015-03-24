dc6bcc5dd1cebbda0c1db0576c5b19f0bfe4106f
Jonathan <modjow.jm9@gmail.com>
2015-03-24 21:39:04 +0100

    Fix

--- app/controller/NotificationController.php
+++ app/controller/NotificationController.php
@@ -6,6 +6,34 @@ use \lib\HTTPResponse;
 
 class NotificationController extends \lib\Controller {
 
+	//generic php function to send GCM push notification
+	function sendPushNotificationToGCM($registatoin_ids, $message) {
+		//Google cloud messaging GCM-API url
+		$url = 'https://android.googleapis.com/gcm/send';
+		$fields = array(
+			'registration_ids' => $registatoin_ids,
+			'data' => $message,
+		); 
+		$headers = array(
+			'Authorization: key=' . $this->_app->_config->get('google_api_key'),
+			'Content-Type: application/json'
+		);
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+		$result = curl_exec($ch);      
+		if ($result === FALSE) {
+			die('Curl failed: ' . curl_error($ch));
+		}
+		curl_close($ch);
+		return $result;
+	}
+
 	/**
 	 * Post a notification
 	 * @uri    /notification
@@ -78,37 +106,5 @@ class NotificationController extends \lib\Controller {
 		}
 
 		HTTPResponse::send(json_encode($json));
-	}
-
-
-
-
-
-	//generic php function to send GCM push notification
-	function sendPushNotificationToGCM($registatoin_ids, $message) {
-		//Google cloud messaging GCM-API url
-		$url = 'https://android.googleapis.com/gcm/send';
-		$fields = array(
-			'registration_ids' => $registatoin_ids,
-			'data' => $message,
-		); 
-		$headers = array(
-			'Authorization: key=' . $this->_app->_config->get('google_api_key'),
-			'Content-Type: application/json'
-		);
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_POST, true);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
-		$result = curl_exec($ch);      
-		if ($result === FALSE) {
-			die('Curl failed: ' . curl_error($ch));
-		}
-		curl_close($ch);
-		return $result;
 	}
 }
