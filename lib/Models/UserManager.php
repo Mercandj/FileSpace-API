9e7b902b327aad7999e5a29cbf7a3dcc6dd7a8ee
Jonathan <modjow.jm9@gmail.com>
2015-03-30 23:18:57 +0200

    [User] fix getAll sizeFiles

--- lib/Models/UserManager.php
+++ lib/Models/UserManager.php
@@ -99,7 +99,7 @@ class UserManager extends \lib\Manager {
 
 	public function getAll() {
 		$users = [];
-		$req = $this->_db->prepare('SELECT id,username,password,last_name,first_name,email,date_creation,date_last_connection,android_id,(SELECT COUNT(*) FROM file WHERE file.id_user = user.id) AS num_files,(SELECT COUNT(size) FROM file WHERE file.id_user = user.id) AS size_files FROM user');
+		$req = $this->_db->prepare('SELECT id,username,password,last_name,first_name,email,date_creation,date_last_connection,android_id,(SELECT COUNT(*) FROM file WHERE file.id_user = user.id) AS num_files,(SELECT SUM(size) FROM file WHERE file.id_user = user.id) AS size_files FROM user');
 		$req->execute();
     	while ($donnees = $req->fetch(\PDO::FETCH_ASSOC))
 	    	$users[] = new User($donnees);
