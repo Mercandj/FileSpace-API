401c5e46fe940ec1370ded00c487052e9b9dd56e
Jonathan <modjow.jm9@gmail.com>
2015-04-12 22:45:06 +0200

    fread 1mo

--- app/controller/FileController.php
+++ app/controller/FileController.php
@@ -571,8 +571,8 @@ class FileController extends \lib\Controller {
 					header('Connection: close');
 
 					//readfile($file_name);		// push it out
-					// set the download rate limit (=> 10000 kb/s)
-					$download_rate = 10000;
+					// set the download rate limit (=> 1000 kb/s)
+					$download_rate = 1000;
 					flush();
 				    $file = fopen($file_name, "r");
 				    while(!feof($file))
