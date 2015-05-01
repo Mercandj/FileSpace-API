8d6e8d12e72775a6f0a47e429b69b7fbada637b7
Mercandj <modjow.jm9@gmail.com>
2015-05-01 12:11:08 +0100

    Update RoboticsController.php

--- app/controller/RoboticsController.php
+++ app/controller/RoboticsController.php
@@ -15,18 +15,25 @@ class RoboticsController extends \lib\Controller {
 	 * @return JSON with info about robotics
 	 */
 	public function get($pin_id) {
+		$json['succeed'] = false;
+		
+		$id_user = $this->_app->_config->getId_user();
+		$userManager = $this->getManagerof('User');
+		$user = $userManager->getById($id_user);
 
-		$response = file_get_contents($this->_app->_config->get('server_robotics') . "/api/v1/pin/".$pin_id);
-
-		$json['succeed'] = true;
-		$json['result'] = array(
-
-			array(
-				"title" => "response",
-				"value" => "".$response
-			)
-
-		);
+		if($user->isAdmin()) {
+			$response = file_get_contents($this->_app->_config->get('server_robotics') . "/api/v1/pin/".$pin_id);
+			$json['succeed'] = true;
+			$json['result'] = array(
+				array(
+					"title" => "response",
+					"value" => "".$response
+				)
+			);	
+		}
+		else {
+			$json['toast'] = 'Unauthorized access.';
+		}
 
 		HTTPResponse::send(json_encode($json));
 	}
@@ -38,34 +45,46 @@ class RoboticsController extends \lib\Controller {
 	 * @method 	POST
 	 */
 	public function post($pin_id) {
+		$json['succeed'] = false;
+		
+		$id_user = $this->_app->_config->getId_user();
+		$userManager = $this->getManagerof('User');
+		$user = $userManager->getById($id_user);
 
-		$value = '0';
-
-		if(HTTPRequest::postExist('value'))
-			$value = HTTPRequest::postData('value');
-
-		$url = $this->_app->_config->get('server_robotics') . "/api/v1/pin/".$pin_id;
-		$data = array('value' => $value);
-
-		$options = array(
-		    'http' => array(
-		        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-		        'method'  => 'PATCH',
-		        'content' => http_build_query($data),
-		    )
-		);
-		$context  = stream_context_create($options);
-		$response = file_get_contents($url, false, $context);
-
-		$json['succeed'] = true;
-		$json['result'] = array(
-
-			array(
-				"title" => "response",
-				"value" => "".$response
-			)
-
-		);
+		if($user->isAdmin()) {
+			$value = '0';
+	
+			if(HTTPRequest::postExist('value'))
+				$value = HTTPRequest::postData('value');
+				
+			$servo = false;
+			if(HTTPRequest::postExist('servo'))
+				$servo = HTTPRequest::postData('servo');
+	
+			$url = $this->_app->_config->get('server_robotics') . "/api/v1/pin/".$pin_id;
+			$data = array('value' => $value);
+	
+			$options = array(
+			    'http' => array(
+			        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+			        'method'  => 'PATCH',
+			        'content' => http_build_query($data),
+			    )
+			);
+			$context  = stream_context_create($options);
+			$response = file_get_contents($url, false, $context);
+	
+			$json['succeed'] = true;
+			$json['result'] = array(
+				array(
+					"title" => "response",
+					"value" => "".$response
+				)
+			);
+		}
+		else {
+			$json['toast'] = 'Unauthorized access.';
+		}
 
 		HTTPResponse::send(json_encode($json));
 	}
