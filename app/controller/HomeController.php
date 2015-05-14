ab2dad3d2b1146c4459938fbff86b23af1be28a7
Jonathan <modjow.jm9@gmail.com>
2015-05-14 21:44:22 +0200

    Fix file ext

--- app/controller/HomeController.php
+++ app/controller/HomeController.php
@@ -17,34 +17,34 @@ class HomeController extends \lib\Controller {
 	 * @return 	JSON List of home data
 	 */
 	public function get() {
-	  $json['succeed'] = false;
-    
-    // Get Manager
-    $userManager = $this->getManagerof('User');
-    $fileManager = $this->getManagerof('File');
-    $conversationManager = $this->getManagerof('Conversation');
+		$json['succeed'] = false;
+
+		// Get Manager
+		$userManager = $this->getManagerof('User');
+		$fileManager = $this->getManagerof('File');
+		$conversationManager = $this->getManagerof('Conversation');
 		$conversationUserManager = $this->getManagerof('ConversationUser');
 		$conversationMessageManager = $this->getManagerof('ConversationMessage');
 
-	  // Get User
-	  $id_user = $this->_app->_config->getId_user();
-	  $user = $userManager->getById($id_user);
-	  
-	  // Get parameters
-	  $id_file_parent = -1;
+		// Get User
+		$id_user = $this->_app->_config->getId_user();
+		$user = $userManager->getById($id_user);
+
+		// Get parameters
+		$id_file_parent = -1;
 		if(HTTPRequest::getExist('id_file_parent'))
 			$id_file_parent = HTTPRequest::getData('id_file_parent');
-	  
-	  // Get data from database
-    $my_recent_files = [];
-    $public_recent_files = [];
-    $recent_messages = [];
-	  $list_my_recent_files = $fileManager->getByParentId($id_user, $id_file_parent);
-	  $list_public_recent_files = $fileManager->getPublic();
-	  $list_conversationMessage = $conversationMessageManager->getAllByConversationId($id);
-	  
-	  // Format data
-	  foreach ($list_my_recent_files as $file) {
+
+		// Get data from database
+		$my_recent_files = [];
+		$public_recent_files = [];
+		$recent_messages = [];
+		$list_my_recent_files = $fileManager->getByParentId($id_user, $id_file_parent);
+		$list_public_recent_files = $fileManager->getPublic();
+		$list_conversationMessage = $conversationMessageManager->getAllByConversationId($id);
+
+		// Format data
+		foreach ($list_my_recent_files as $file) {
 			$my_recent_files[] = $file->toArray();
 		}
 		foreach ($list_public_recent_files as $file) {
@@ -59,19 +59,19 @@ class HomeController extends \lib\Controller {
 			$recent_messages[] = $tmp_array;
 		}
 		$result = array(
-        "my_recent_files" => $my_recent_files,
-        "public_recent_files" => $public_recent_files,
-        "recent_messages"  => $recent_messages
-    );
-    
-    if($user->isAdmin()) {
-	  
-	  }
-	
-	  // Return data
-	  $json['result'] = $result;
-	  $json['succeed'] = true;
-	  HTTPResponse::send(json_encode($json));
+			"my_recent_files" => $my_recent_files,
+			"public_recent_files" => $public_recent_files,
+			"recent_messages"  => $recent_messages
+		);
+
+		if($user->isAdmin()) {
+
+		}
+
+		// Return data
+		$json['result'] = $result;
+		$json['succeed'] = true;
+		HTTPResponse::send(json_encode($json));
 	}
 	
 }
