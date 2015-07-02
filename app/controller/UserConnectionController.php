d25bc805b2972a16eb2b5d06321ca2542dcd232a
Mercandj <modjow.jm9@gmail.com>
2015-07-02 16:40:34 +0100

    Update UserConnectionController.php

--- app/controller/UserConnectionController.php
+++ app/controller/UserConnectionController.php
@@ -26,9 +26,13 @@ class UserConnectionController extends \lib\Controller {
 		$userConnectionManager = $this->getManagerof('UserConnection');
 
 		if($user->isAdmin()) {			
-			$list_user_connetion = $userConnectionManager->getAllPage(250, 1);
+			$list_user_connetion = $userConnectionManager->getAllPage(300, 1);
 			foreach ($list_user_connetion as $user_connetion) {
-				$result[] = $user_connetion->toArray();
+				$user_connetion_array = $user_connetion->toArray();
+				$user_array[] = $userManager->getById($user_connetion->getId_user());
+				if(count($user_array)>0)
+					$user_connetion_array['username'] = $user_array[0]->getUsername();
+				$result[] = $user_connetion_array;
 			}
 			$json['result'] = $result;
 			$json['succeed'] = true;
