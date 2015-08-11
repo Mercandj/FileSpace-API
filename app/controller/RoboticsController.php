40ca2f0c56c59d6a668290ced6c6be5ef5364901
Jonathan <modjow.jm9@gmail.com>
2015-08-11 11:49:19 +0200

    Removed robotics GET

--- app/controller/RoboticsController.php
+++ app/controller/RoboticsController.php
@@ -95,54 +95,12 @@ class RoboticsController extends \lib\Controller {
 
 
 
-
-	/**
-	 * Robotics : test
-	 * @uri    /robotics
-	 * @method GET
-	 * @return JSON with info about robotics
-	 */
-	public function get_test() {
-		$json['succeed'] = false;
-		
-		$id_user = $this->_app->_config->getId_user();
-		$userManager = $this->getManagerof('User');
-		$user = $userManager->getById($id_user);
-
-		if($user->isAdmin()) {
-
-			$url = $this->_app->_config->get('server_robotics_2')."?username=" . $user->getUsername();
-			
-			if(HTTPRequest::getExist('mesure_id'))
-				$url .= '&mesure_id=' . HTTPRequest::getData('mesure_id');
-			else if(HTTPRequest::getExist('ordre_id'))
-				$url .= '&ordre_id=' . HTTPRequest::getData('ordre_id');
-
-			if(HTTPRequest::getExist('value'))
-				$url .= '&value=' . HTTPRequest::getData('value');
-
-			$response = file_get_contents($url);
-			$json['succeed'] = true;
-			$json['result'] = array(
-				array(
-					"title" => "response",
-					"value" => "".$response
-				)
-			);	
-		}
-		else {
-			$json['toast'] = 'Unauthorized access.';
-		}
-
-		HTTPResponse::send(json_encode($json));
-	}
-
 	/**
 	 * Do robotics actions
 	 * @uri    	/robotics
 	 * @method 	POST
 	 */
-	public function post_test() {
+	public function raspberry() {
 		$json['succeed'] = false;
 		
 		$id_user = $this->_app->_config->getId_user();
