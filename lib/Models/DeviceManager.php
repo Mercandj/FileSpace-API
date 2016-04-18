2387f1593d92c02b4f46b946544d2cbdeb461110
Jonathan <modjow.jm9@gmail.com>
2016-04-18 23:34:06 +0200

    Add field.

--- lib/Models/DeviceManager.php
+++ lib/Models/DeviceManager.php
@@ -25,6 +25,7 @@ class DeviceManager extends \lib\Manager {
 		$to_insert['android_device_display_language'] 		= $device->getAndroid_device_display_language();
 		$to_insert['android_device_country'] 				= $device->getAndroid_device_country();
 		$to_insert['android_device_version_sdk'] 			= $device->getAndroid_device_version_sdk();
+		$to_insert['android_device_version_year'] 			= $device->getAndroid_device_year();
 
 		$req_str = 'INSERT INTO `device` (';
 		$numItems = count($to_insert);
@@ -62,6 +63,7 @@ class DeviceManager extends \lib\Manager {
 		$req->bindParam(':android_device_display_language',		$to_insert['android_device_display_language'],		\PDO::PARAM_STR);
 		$req->bindParam(':android_device_country',				$to_insert['android_device_country'],				\PDO::PARAM_STR);
 		$req->bindParam(':android_device_version_sdk',			$to_insert['android_device_version_sdk'],			\PDO::PARAM_STR);
+		$req->bindParam(':android_device_version_year',			$to_insert['android_device_version_year'],			\PDO::PARAM_STR);
 		$req->execute();
 		$req->closeCursor();
 	}
